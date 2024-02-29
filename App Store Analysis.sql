265ce4420fabf58e4a91760d6858235bc656e55f
Andy Chen <137304001+andychn10@users.noreply.github.com>
2024-02-29 12:43:21 -0500

    App Store Analysis.sql

--- App Store Analysis.sql
+++ App Store Analysis.sql
@@ -34,93 +34,93 @@ WHERE app_desc IS null
 
 -- Find the number of apps per genre --
 
-SELECT prime_genre, COUNT(*) as NumApps
+SELECT prime_genre, COUNT(*) AS NumApps
 FROM AppleStore
-group by prime_genre
-ORDER by NumApps DESC
+GROUP BY prime_genre
+ORDER BY NumApps DESC
 
 -- Summarize app ratings --
 
 SELECT min(user_rating) AS MinRating,
 	   max(user_rating) AS MaxRating,
-       avg(user_rating) as AvgRating
+       avg(user_rating) AS AvgRating
 FROM AppleStore
 
 -- Basic summary of paid apps --
 
-SELECT min(price) as MinPrice,
-	   max(price) as MaxPrice,
-       avg(price) as AvgPrice
-from AppleStore
-where price > 0
+SELECT min(price) AS MinPrice,
+	   max(price) AS MaxPrice,
+       avg(price) AS AvgPrice
+FROM AppleStore
+WHERE price > 0
 
 -- Distribution of App Prices --
 
 SELECT
-	(price / 2) *2 as PriceBinStart,
-    ((price / 2) *2) +2 as PriceBinEnd,
-    count(*) as NumApps
-From AppleStore
-GROUP by PriceBinStart
-order by PriceBinStart
+	(price / 2) *2 AS PriceBinStart,
+    ((price / 2) *2) +2 AS PriceBinEnd,
+    COUNT(*) AS NumApps
+FROM AppleStore
+GROUP BY PriceBinStart
+ORDER BY PriceBinStart
 
-** DATA ANALYSIS **
+** ANALYSIS **
 
 -- Determine whether paid apps have higher ratings --
 
-select CASE
-			when price > 0 then 'Paid App'
-            else 'Free App'
-       end as App_Type,
-       avg(user_rating) as Avg_Rating
-from AppleStore
-Group by App_Type
+SELECT CASE
+	   WHEN price > 0 THEN 'Paid App'
+           ELSE 'Free App'
+       END AS App_Type,
+       avg(user_rating) AS Avg_Rating
+FROM AppleStore
+GROUP BY App_Type
 
 -- Check if apps with more supported languages have higher ratings --
 
-select CASE
-			when lang_num < 10 then 'Less than 10 languages'
-            when lang_num between 10 and 15 then '10 - 20 languages'
-            when lang_num between 20 and 25 then '20 - 30 languages'
-            else 'Greater than 30 languages'
-       end as language_basket,
-       avg(user_rating) as Avg_Rating
-from AppleStore
-group by language_basket
-order by Avg_Rating DESC
+SELECT CASE
+	    WHEN lang_num < 10 THEN 'Less than 10 languages'
+            WHEN lang_num BETWEEN 10 AND 15 THEN '10 - 20 languages'
+            WHEN lang_num BETWEEN 20 AND 25 THEN '20 - 30 languages'
+            ELSE 'Greater than 30 languages'
+       END AS language_basket,
+       avg(user_rating) AS Avg_Rating
+FROM AppleStore
+GROUP BY language_basket
+ORDER BY Avg_Rating DESC
 
 -- Check genres with low ratings --
 
-select prime_genre,
-	   avg(user_rating) as Avg_Rating
-from AppleStore
-group by prime_genre
-Order by Avg_Rating ASC
-limit 5
+SELECT prime_genre,
+       avg(user_rating) AS Avg_Rating
+FROM AppleStore
+GROUP BY prime_genre
+ORDER BY Avg_Rating ASC
+LIMIT 5
 
 -- Check genres with high ratings --
-select prime_genre,
-	   avg(user_rating) as Avg_Rating
-from AppleStore
-group by prime_genre
-order by Avg_Rating DESC
-Limit 5
+SELECT prime_genre,
+       avg(user_rating) AS Avg_Rating
+FROM AppleStore
+GROUP BY prime_genre
+ORDER BY Avg_Rating DESC
+LIMIT 5
 
 -- Check top-rated apps for each genre --
 
-Select
-	prime_genre,
-    track_name,
-    user_rating
-from (
-  	  SELECT
+SELECT
+      prime_genre,
+      track_name,
+      user_rating
+FROM (
+      SELECT
       prime_genre,
       track_name,
       user_rating,
-      RANK() OVER(PARTITION BY prime_genre order by user_rating desc, rating_count_tot desc) as rank
-      from
+      RANK() OVER(PARTITION BY prime_genre ORDER BY user_rating DESC, rating_count_tot DESC) AS rank
+      FROM
       AppleStore
-    ) as a
-where 
+    ) AS a
+WHERE 
 a.rank = 1
  
